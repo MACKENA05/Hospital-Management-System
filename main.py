0264cb6cb53ee9193080dd0cb3265d6aca55a37e
MACKENA05 <mackenamuthui@gmail.com>
2025-03-13 12:57:26 +0300

    updated the validating date function

--- main.py
+++ main.py
@@ -2,14 +2,12 @@ import click
 from models import session, Department, Doctor, Patient, Appointment
 from datetime import datetime
 
-# validation
+# Validation
 def validate_date(date):
     try:
         return datetime.strptime(date, "%Y-%m-%d %H:%M")
     except ValueError:
         return None
-    
-
 
 # Add Department
 def add_department(name):
@@ -20,92 +18,93 @@ def add_department(name):
         click.echo(f"'{name}' department added successfully.")
     except Exception as e:
         session.rollback()
-        click.echo("Error adding department as {e}")
+        click.echo(f"Error adding department: {e}")
     finally:
-        session.close
+        session.close()
 
-#Adding doctors
-def add_doctor(name,department_id):
+# Adding Doctors
+def add_doctor(name, department_id):
     try:
-        department = session.query(Department).filter_by(department_id = department_id).one()
-        doctor = Doctor(doctor_name = name, department_id=department_id)
+        department = session.query(Department).filter_by(department_id=department_id).one()
+        doctor = Doctor(doctor_name=name, department_id=department_id)
         session.add(doctor)
         session.commit()
-        click.echo(f"'{name}' added successfuly in '{department.department_name}' department")
+        click.echo(f"'{name}' added successfully in '{department.department_name}' department")
     except Exception as e:
-        session.roll()
+        session.rollback()
         click.echo(f'Error adding Doctor: {e}')
     finally:
         session.close()
 
-#adding patients
+# Adding Patients
 def add_patient(name, phone_number):
     try:
-        patient = Patient(patient_name = name, phone_number=phone_number)
+        patient = Patient(patient_name=name, phone_number=phone_number)
         session.add(patient)
         session.commit()
         click.echo(f"Patient {name} added successfully")
     except Exception as e:
         session.rollback()
-        click.echo(f"Error adding patient: {e} ")
+        click.echo(f"Error adding patient: {e}")
     finally:
         session.close()
-#scheduling an appointment
-def schedule_appointment(doctor_id, patient_id,date_time):
+
+# Scheduling an Appointment
+def schedule_appointment(doctor_id, patient_id, date_time):
     date_time = validate_date(date_time)
     if not date_time:
-        click.echo("Invalid date format.Use 'YYYY-MM-DD HH:MM'.")
+        click.echo("Invalid date format. Use 'YYYY-MM-DD HH:MM'.")
         return
     try:
         doctor = session.query(Doctor).filter_by(doctor_id=doctor_id).one()
         patient = session.query(Patient).filter_by(patient_id=patient_id).one()
-        appointment = Appointment(doctor_id=doctor_id,patient_id=patient_id, appointment_date=date_time)
+        appointment = Appointment(doctor_id=doctor_id, patient_id=patient_id, appointment_date=date_time)
         session.add(appointment)
         session.commit()
-        click.echo(f"Appointment for {patient.patient_name}  scheduled on {date_time} with {doctor.doctor_name} added successfully ")
+        click.echo(f"Appointment for {patient.patient_name} scheduled on {date_time.strftime('%Y-%m-%d %H:%M')} with {doctor.doctor_name} added successfully")
     except Exception as e:
         session.rollback()
         click.echo(f"Error scheduling the appointment: {e}")
     finally:
         session.close()
 
-#viewing all departments
+# Viewing All Departments
 def view_departments():
     departments = session.query(Department).all()
     if not departments:
-        click.echo("No department found")
+        click.echo("No departments found")
     else:
         for department in departments:
-            click.echo(f"{department.department_id} Name:{department.department_name}")
-    
-#View  all doctors
+            click.echo(f"{department.department_id}  Name: {department.department_name}")
+
+# Viewing All Doctors
 def view_doctors():
     doctors = session.query(Doctor).all()
     if not doctors:
         click.echo("No doctors found")
     else:
         for doctor in doctors:
-            click.echo(f"{doctor.doctor_id}. Name:{doctor.doctor_name} Department:{doctor.department_id}")
+            click.echo(f"{doctor.doctor_id}.  Name: {doctor.doctor_name}  Department: {doctor.department_id}")
 
-#view all patients
+# Viewing All Patients
 def view_patients():
     patients = session.query(Patient).all()
     if not patients:
-        click.echo("No patient found")
+        click.echo("No patients found")
     else:
         for patient in patients:
-            click.echo(f"{patient.patient_id}  Name: {patient.patient_name}  PhoneNumber:{patient.phone_number}")
+            click.echo(f"{patient.patient_id}  Name: {patient.patient_name}  Phone Number: {patient.phone_number}")
 
-#view all appointments
+# Viewing All Appointments
 def view_appointments():
     appointments = session.query(Appointment).all()
     if not appointments:
         click.echo("No appointments found")
     else:
         for appointment in appointments:
-            click.echo(f"{appointment.appointment_id}, Doctor ID: {appointment.doctor_id}, Patient ID: {appointment.patient_id}, Date: {appointment.appointment_date}")
+            click.echo(f"{appointment.appointment_id},  Doctor ID: {appointment.doctor_id},  Patient ID: {appointment.patient_id},  Date: {appointment.appointment_date.strftime('%Y-%m-%d %H:%M')}")
 
-#Delete department
+# Delete Department
 def delete_department(department_id):
     try:
         department = session.query(Department).filter_by(department_id=department_id).one()
@@ -114,11 +113,11 @@ def delete_department(department_id):
         click.echo(f"{department.department_name} department deleted successfully")
     except Exception as e:
         session.rollback()
-        click.echo("Error deleting the department")
+        click.echo(f"Error deleting the department: {e}")
     finally:
         session.close()
 
-#delete doctor
+# Delete Doctor
 def delete_doctor(doctor_id):
     try:
         doctor = session.query(Doctor).filter_by(doctor_id=doctor_id).one()
@@ -127,11 +126,11 @@ def delete_doctor(doctor_id):
         click.echo(f"{doctor.doctor_name} deleted successfully")
     except Exception as e:
         session.rollback()
-        click.echo("Error deleting the doctor")
+        click.echo(f"Error deleting the doctor: {e}")
     finally:
         session.close()
 
-#delete patient
+# Delete Patient
 def delete_patient(patient_id):
     try:
         patient = session.query(Patient).filter_by(patient_id=patient_id).one()
@@ -140,11 +139,11 @@ def delete_patient(patient_id):
         click.echo(f"{patient.patient_name} deleted successfully")
     except Exception as e:
         session.rollback()
-        click.echo("Error deleting the patient")
+        click.echo(f"Error deleting the patient: {e}")
     finally:
         session.close()
 
-#delete appointment
+# Delete Appointment
 def delete_appointment(appointment_id):
     try:
         appointment = session.query(Appointment).filter_by(appointment_id=appointment_id).one()
@@ -153,20 +152,20 @@ def delete_appointment(appointment_id):
         click.echo(f"Appointment {appointment.appointment_id} deleted successfully")
     except Exception as e:
         session.rollback()
-        click.echo("Error deleting the appointment")
+        click.echo(f"Error deleting the appointment: {e}")
     finally:
         session.close()
 
+# Update Appointment
 def update_appointment(appointment_id):
-    """Update an existing appointment."""
     try:
         appointment = session.query(Appointment).filter_by(appointment_id=appointment_id).one()
         click.echo(f"Current Appointment Details:")
         click.echo(f"  Doctor ID: {appointment.doctor_id}")
         click.echo(f"  Patient ID: {appointment.patient_id}")
-        click.echo(f"  Date/Time: {appointment.appointment_date}")
+        click.echo(f"  Date/Time: {appointment.appointment_date.strftime('%Y-%m-%d %H:%M')}")
 
-        # new details prompt
+        # New details prompt
         new_doctor_id = click.prompt("Enter new doctor ID (or press Enter to keep current)", default=appointment.doctor_id, type=int)
         new_patient_id = click.prompt("Enter new patient ID (or press Enter to keep current)", default=appointment.patient_id, type=int)
         new_date_time = click.prompt("Enter new date and time (YYYY-MM-DD HH:MM) (or press Enter to keep current)", default=appointment.appointment_date.strftime("%Y-%m-%d %H:%M"))
@@ -183,59 +182,59 @@ def update_appointment(appointment_id):
         click.echo(f"Appointment ID {appointment_id} updated successfully.")
     except Exception as e:
         session.rollback()
-        click.echo("Error: Invalid data provided.")
+        click.echo(f"Error updating the appointment: {e}")
     finally:
         session.close()
 
-#view doctors in departments
+# View Doctors in Department
 def view_doctors_in_department(department_id):
     try:
         department = session.query(Department).filter_by(department_id=department_id).one()
         doctors = session.query(Doctor).filter_by(department_id=department_id).all()
         if not doctors:
-            click.echo("No doctors found in {department.department_name} department")
+            click.echo(f"No doctors found in {department.department_name} department")
         else:
-            click.echo(f"Doctors in {department.department_name} department")
+            click.echo(f"Doctors in {department.department_name} department:")
             for doctor in doctors:
-                 click.echo(f"Doctor ID:{doctor.doctor_id}, Name: {doctor.doctor_name}")
+                click.echo(f"Doctor ID: {doctor.doctor_id}, Name: {doctor.doctor_name}")
     except Exception as e:
-        click.echo("Error viewing doctors in this department")
+        click.echo(f"Error viewing doctors in this department: {e}")
     finally:
         session.close()
 
-#view appointments for doctors
+# View Appointments for Doctor
 def view_appointments_for_doctor(doctor_id):
     try:
-        doctor = session.query(Doctor).filter_by(doctor_id =doctor_id).one()
+        doctor = session.query(Doctor).filter_by(doctor_id=doctor_id).one()
         appointments = session.query(Appointment).filter_by(doctor_id=doctor_id).all()
         if not appointments:
             click.echo(f"No appointments found for {doctor.doctor_name}")
         else:
-            click.echo(f"Appointments for {doctor.doctor_name}")
+            click.echo(f"Appointments for {doctor.doctor_name}:")
             for appointment in appointments:
-                click.echo(f"Appointment ID:{appointment.appointment_id}, Patient ID: {appointment.patient_id}, Date: {appointment.appointment_date}")
+                click.echo(f"Appointment ID: {appointment.appointment_id}, Patient ID: {appointment.patient_id}, Date: {appointment.appointment_date.strftime('%Y-%m-%d %H:%M')}")
     except Exception as e:
-        click.echo("Error fetching appointments ")
+        click.echo(f"Error fetching appointments: {e}")
     finally:
         session.close()
 
-#view appointments for patient
+# View Appointments for Patient
 def view_appointments_for_patient(patient_id):
     try:
-        patient = session.query(Patient).filter_by(patient_id = patient_id).one()
-        appointments = session.query(Appointment).filter_by(patient_id = patient_id).all()
+        patient = session.query(Patient).filter_by(patient_id=patient_id).one()
+        appointments = session.query(Appointment).filter_by(patient_id=patient_id).all()
         if not appointments:
-            click.echo(f"No appointments for Patient {patient.patient_name}")
+            click.echo(f"No appointments found for Patient {patient.patient_name}")
         else:
-            click.echo(f"Appointments for {patient.patient_name}")
+            click.echo(f"Appointments for {patient.patient_name}:")
             for appointment in appointments:
-                click.echo(f"  Appointment ID: {appointment.appointment_id}, Doctor ID: {appointment.doctor_id}, Date: {appointment.appointment_date}")
+                click.echo(f"  Appointment ID: {appointment.appointment_id}, Doctor ID: {appointment.doctor_id}, Date: {appointment.appointment_date.strftime('%Y-%m-%d %H:%M')}")
     except Exception as e:
-        click.echo("Error getting appointments ")
+        click.echo(f"Error getting appointments: {e}")
     finally:
         session.close()
 
-#List Doctors and Patient for a certain department
+# List Doctors and Appointments for Department
 def list_doctors_and_appointments(department_id):
     try:
         department = session.query(Department).filter_by(department_id=department_id).one()
@@ -254,20 +253,22 @@ def list_doctors_and_appointments(department_id):
             else:
                 for appointment in appointments:
                     patient = session.query(Patient).filter_by(patient_id=appointment.patient_id).one()
-                    click.echo(f"  Appointment ID: {appointment.appointment_id}, Patient: {patient.patient_name}, Date: {appointment.appointment_date}")
+                    click.echo(f"  Appointment ID: {appointment.appointment_id}, Patient: {patient.patient_name}, Date: {appointment.appointment_date.strftime('%Y-%m-%d %H:%M')}")
     except Exception as e:
-        click.echo("Error listing doctors and appointments for a certain department")
+        click.echo(f"Error listing doctors and appointments for a certain department: {e}")
+    finally:
+        session.close()
 
-#search for patient by name
+# Search for Patient by Name
 def search_patient_by_name(name):
-        patients = session.query(Patient).filter(Patient.patient_name.ilike(f"%{name}%")).all()
-        if not patients:
-            click.echo(f"No patient found with name containing {name}")
-        else:
-            for patient in patients:
-                click.echo(f"{patient.patient_id} Name:{patient.patient_name}")
-    
-#search Doctor by name
+    patients = session.query(Patient).filter(Patient.patient_name.ilike(f"%{name}%")).all()
+    if not patients:
+        click.echo(f"No patient found with name containing '{name}'")
+    else:
+        for patient in patients:
+            click.echo(f"{patient.patient_id} Name: {patient.patient_name}")
+
+# Search for Doctor by Name
 def search_doctor_by_name(name):
     doctors = session.query(Doctor).filter(Doctor.doctor_name.ilike(f"%{name}%")).all()
     if not doctors:
@@ -276,8 +277,6 @@ def search_doctor_by_name(name):
         click.echo(f"Doctors found with name containing '{name}':")
         for doctor in doctors:
             click.echo(f"ID: {doctor.doctor_id}, Name: {doctor.doctor_name}, Department ID: {doctor.department_id}")
-
-
 
 # Menu System
 def show_menu():
@@ -306,24 +305,24 @@ def show_menu():
 def menu():
     while True:
         show_menu()
-        choice = click.prompt("Enter your choice", type = int)
+        choice = click.prompt("Enter your choice", type=int)
 
         if choice == 1:
             name = click.prompt("Enter department name")
             add_department(name)
         elif choice == 2:
-            name = click.prompt("Enter Doctors name")
-            department_id = click.prompt("Enter the department ID")
+            name = click.prompt("Enter doctor name")
+            department_id = click.prompt("Enter the department ID", type=int)
             add_doctor(name, department_id)
         elif choice == 3:
             name = click.prompt("Enter patient name")
-            phone_number = click.prompt("Enter patients phone number")
-            add_patient(name,phone_number)
+            phone_number = click.prompt("Enter patient's phone number")
+            add_patient(name, phone_number)
         elif choice == 4:
-            doctor_id = click.prompt("Enter Doctors ID")
-            patient_id = click.prompt("Enter Patient ID")
-            date_time = click.prompt("Enter the Appointment date and time (YYYY-MM-DD HH:MM)")
-            schedule_appointment(doctor_id, patient_id ,date_time)
+            doctor_id = click.prompt("Enter doctor ID", type=int)
+            patient_id = click.prompt("Enter patient ID", type=int)
+            date_time = click.prompt("Enter the appointment date and time (YYYY-MM-DD HH:MM)")
+            schedule_appointment(doctor_id, patient_id, date_time)
         elif choice == 5:
             view_departments()
         elif choice == 6:
@@ -333,31 +332,31 @@ def menu():
         elif choice == 8:
             view_appointments()
         elif choice == 9:
-            department_id = click.prompt("Enter Department ID to delete", type = int)
+            department_id = click.prompt("Enter department ID to delete", type=int)
             delete_department(department_id)
         elif choice == 10:
-            doctor_id = click.prompt("Enter Doctor ID to delete", type = int)
+            doctor_id = click.prompt("Enter doctor ID to delete", type=int)
             delete_doctor(doctor_id)
         elif choice == 11:
-            patient_id = click.prompt("Enter Patient ID to delete", type = int)
+            patient_id = click.prompt("Enter patient ID to delete", type=int)
             delete_patient(patient_id)
         elif choice == 12:
-            appointment_id = click.prompt("Enter Appointment ID to delete", type = int)
+            appointment_id = click.prompt("Enter appointment ID to delete", type=int)
             delete_appointment(appointment_id)
         elif choice == 13:
-            appointment_id = click.prompt("Enter Appointment ID to update")
+            appointment_id = click.prompt("Enter appointment ID to update", type=int)
             update_appointment(appointment_id)
         elif choice == 14:
-            department_id = click.prompt("Enter Department ID", type=int)
+            department_id = click.prompt("Enter department ID", type=int)
             view_doctors_in_department(department_id)
         elif choice == 15:
-            doctor_id = click.prompt("Enter Doctor ID", type=int) 
+            doctor_id = click.prompt("Enter doctor ID", type=int)
             view_appointments_for_doctor(doctor_id)
         elif choice == 16:
-            patient_id = click.prompt("Enter Patient ID", type=int)
+            patient_id = click.prompt("Enter patient ID", type=int)
             view_appointments_for_patient(patient_id)
         elif choice == 17:
-            department_id = click.prompt("Enter Department ID", type=int)
+            department_id = click.prompt("Enter department ID", type=int)
             list_doctors_and_appointments(department_id)
         elif choice == 18:
             name = click.prompt("Enter patient name to search")
@@ -366,11 +365,10 @@ def menu():
             name = click.prompt("Enter doctor name to search")
             search_doctor_by_name(name)
         elif choice == 20:
-            click.echo("Ending the session .....")
+            click.echo("Ending the session...")
             break
-
         else:
-            click.echo("Invalid choice.Try again")
+            click.echo("Invalid choice. Try again.")
 
 # Click Group
 @click.group()
@@ -378,7 +376,7 @@ def cli():
     """Hospital Management System CLI"""
     pass
 
-#menu command
+# Menu Command
 @cli.command()
 def start_menu():
     menu()
